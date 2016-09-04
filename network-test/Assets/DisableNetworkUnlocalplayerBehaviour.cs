1cb2949eddabeaa9521fb62e0ba39f1af5f5363e
kouhei <kouhei140309@outlook.jp>
2016-09-04 22:24:31 +0900

    http://tsubakit1.hateblo.jp/entry/2015/06/13/233000を参考にネットワーク処理の作成

---
+++ network-test/Assets/DisableNetworkUnlocalplayerBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class DisableNetworkUnlocalplayerBehaviour : NetworkBehaviour {
+    [SerializeField]
+    Behaviour[] behaviours;
+
+	void Start () {
+	    if (!this.isLocalPlayer)
+        {
+            foreach (var b in behaviours)
+            {
+                b.enabled = false;
+            }
+        }
+	}
+	
+    void OnApplicationFocus(bool focusState)
+    {
+        if (this.isLocalPlayer)
+        {
+            foreach (var b in behaviours)
+            {
+                b.enabled = focusState;
+            }
+        }
+    }
+}
