f386cfcb9415876548d168af837dd69638a3ee7b
kouhei <kouhei140309@outlook.jp>
2016-09-24 22:49:29 +0900

    プレイヤーの射撃処理
    HPの減少

---
+++ network-test/Assets/Script/PlayerHealth.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class PlayerHealth : NetworkBehaviour {
+
+	[SyncVar(hook = "OnHealthChanged")]
+	private int health = 100;
+
+	private Text healthText;
+
+	void Start()
+	{
+		// Textオブジェクトをキャッシュ
+		healthText = GameObject.Find("Health Text").GetComponent<Text>();
+		if (healthText == null) Debug.LogError("HealthText is Null!");
+		SetHealthText();
+	}
+
+	void SetHealthText()
+	{
+		if (!isLocalPlayer) return;
+		healthText.text = "Health " + health.ToString();
+	}
+
+	// ダメージを受けた時のメソッド
+	public void DeductHealth(int dmg)
+	{
+		health -= dmg;
+	}
+
+	// hookメソッド
+	void OnHealthChanged(int hlth)
+	{
+		// 全クライアントへ変更結果を送信
+		health = hlth;
+		// HPの表示を更新
+		SetHealthText();
+	}
+}
---
+++ network-test/Assets/Script/PlayerShooting.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerShooting :  NetworkBehaviour{
+
+	private int damage = 25;
+	private float range = 200; // Raycastの距離
+	// FirstPersonCharacterを指定
+	[SerializeField]
+	private Transform cameraTransform;
+
+	private RaycastHit hit;
+
+	void Update()
+	{
+		CheckIfShooting();
+	}
+
+	void CheckIfShooting()
+	{
+		if (!isLocalPlayer) return;
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Shooting();
+		}
+	}
+
+	void Shooting()
+	{
+		// カメラの前方にRaycastを飛ばす
+		// TransformPoint: 指定した分だけ座標をずらす
+		if (Physics.Raycast(cameraTransform.TransformPoint(0,0, 0.5f), cameraTransform.forward, out hit, range))
+		{
+			Debug.Log("hit:" + hit.transform.tag);
+			// RaycastとPlayerが衝突したとき
+			if (hit.transform.tag == "Player")
+			{
+				// 名前を取得
+				string uIdentity = hit.transform.name;
+				// 名前とダメージ量を引数にメソッド実行
+				CmdTellServerWhoWasShow(uIdentity, damage);
+			}
+		}
+	}
+
+	[Command]
+	void CmdTellServerWhoWasShow(string uniqueID, int dmg)
+	{
+		// 敵プレイヤーの名前でGameObjectを取得
+		GameObject go = GameObject.Find(uniqueID);
+		// ダメージを与える
+		PlayerHealth  playerHealth = go.GetComponent<PlayerHealth>();
+		if (playerHealth == null)
+		{
+			Debug.LogError("playerHealth is Null!");
+			return;
+		}
+		playerHealth.DeductHealth(dmg);
+
+	}
+}
