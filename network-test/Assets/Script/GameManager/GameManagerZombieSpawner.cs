08e1b21e4b564e750bf978d2245c677799ea3ae9
kouhei <kouhei140309@outlook.jp>
2016-10-02 11:32:05 +0900

    ゾンビ出現、移動処理作成

---
+++ network-test/Assets/Script/GameManager/GameManagerZombieSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class GameManagerZombieSpawner : NetworkBehaviour {
+	[SerializeField]
+	GameObject zombiePrefab;
+	[SerializeField]
+	GameObject zombieSpawn;
+
+	private int counter;
+	private int numberOfZombie = 10;
+
+	public override void OnStartServer()
+	{
+		for(int i = 0; i < numberOfZombie; i++)
+		{
+			SpawnZombies();
+		}
+	}
+
+	void SpawnZombies()
+	{
+		// counter
+
+		GameObject zombie = GameObject.Instantiate(zombiePrefab, zombieSpawn.transform.position, Quaternion.identity) as GameObject;
+		NetworkServer.Spawn(zombie);
+	}
+}
---
+++ network-test/Assets/Script/Zombie/ZombieTarget.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class ZombieTarget : NetworkBehaviour {
+	private NavMeshAgent agent;
+	private Transform myTransform;
+	private Transform targetTransform;
+	// ゾンビが探知するレイヤー
+	private LayerMask raycastLayer;
+	// ゾンビがプレイヤーを探知する半径
+	private float radius = 100f;
+
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		GameManagerRefarences.NullCheck(agent, "agent");
+		myTransform = this.transform;
+		raycastLayer = 1 << LayerMask.NameToLayer("Player");
+	}
+
+	void FixedUpdate()
+	{
+		SearchForTarger();
+		MoveForTaget();
+	}
+
+	void SearchForTarger()
+	{
+		// サーバーじゃなければメソッド終了
+		if (!isServer)
+		{
+			return;
+		}
+		// プレイヤーをまだ取得していないとき
+		if (targetTransform == null)
+		{
+			// Physics.OverlapSphere: ある地点を中心に球を作り、衝突したオブジェクトを取得する
+			// 第一引数: 中心点,第二引数: 半径, 第三引数: 対象レイヤー
+			Collider[] hitColliders = Physics.OverlapSphere(myTransform.position, radius, raycastLayer);
+			if (hitColliders.Length > 0)
+			{
+				// プレイヤーを発見した
+				int randomInt = Random.Range(0, hitColliders.Length); // 複数発見した時はランダムに取得
+				targetTransform = hitColliders[randomInt].transform;
+			}
+		}
+		if (targetTransform != null)
+		{
+			BoxCollider box = null;
+			for (int i = 0; i < 10; i++)
+			{
+				box = targetTransform.GetComponent<BoxCollider>();
+				if (GameManagerRefarences.NullCheck(box, "targetBoxCollinder"))
+				{
+					Debug.Log("TargetName:" + targetTransform.gameObject.name);
+					return;
+				}
+
+			}
+			if (box.enabled == false)
+			{
+				targetTransform = null;
+			}
+		}
+	}
+
+	void MoveForTaget()
+	{
+		if (!isServer) return;
+		// プレイヤーオブジェクトを取得済みのとき
+		if (targetTransform != null)
+		{
+			SetNavDestination(targetTransform);
+		}
+	}
+
+	void SetNavDestination(Transform dest)
+	{
+		agent.SetDestination(dest.position);
+	}
+}
